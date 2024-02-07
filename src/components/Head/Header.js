1692b071e1404258ce1faf3d359f3d4d7975051c
Vinnath Satharasinghe <vinnathninura@gmail.com>
2024-02-07 23:15:40 +0530

    add my old project

---
+++ src/components/Head/Header.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import styles from "./Header.module.scss";
+
+
+const Header = () => {
+
+
+    return (
+       
+    );
+};
+
+
+
+export default Header;
